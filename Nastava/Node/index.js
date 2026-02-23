dd76516f85f96cdff4bdb74449d1dd5e69ba20ff
MarkoSlobodjan <marko.slobodjan4@gmail.com>
2026-02-23 17:19:40 +0100

    node js ,npm

---
+++ Nastava/Node/index.js
@@ -0,0 +1,17 @@
+// console.log("NodeJS radi");
+
+import { writeFile } from 'node:fs';
+import { Buffer } from 'node:buffer';
+import { readFile } from 'node:fs';
+
+// const data = new Uint8Array(Buffer.from('Hello Node.js'));
+// writeFile('message.txt', data, (err) => {
+//   if (err) throw err;
+//   console.log('The file has been saved!');
+// });
+
+
+readFile('./message.txt',"utf-8" ,(err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
---
+++ Nastava/npm/index.js
@@ -0,0 +1,7 @@
+import generateName from "sillyName"
+
+// var generateName = require('sillyname');
+var sillyName = generateName();
+
+console.log(`Moje ime je ${sillyName} `);
+
